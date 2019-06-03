b953aaa7aedc0165a0fd32679e5bcd7a676bcec2
Lakshya Agrawal <lavi061997@gmail.com>
2019-06-04 01:42:19 +0530

    submit

--- resources/user.py
+++ resources/user.py
@@ -25,7 +25,6 @@ class UserRegister(Resource):
                 users = users.paginate(int(request.args.get('page')), int(request.args.get('limit')), False)
             else:
                 users = users.paginate(int(request.args.get('page')), 5, False)
-            return
         result = users_schema.dump(users)
         return jsonify(result.data)
 
