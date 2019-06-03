58a6b1e2251e264531a9ac34ed6fd9ae22caeb39
Lakshya Agrawal <lavi061997@gmail.com>
2019-06-03 23:58:50 +0530

    submit

--- models/schema.py
+++ models/schema.py
@@ -1,3 +1,4 @@
+from ma import ma
 class UserSchema(ma.Schema):
     class Meta:
         fields = ("id", "first_name", "last_name", "company_name", "city", "state", "zip", "website", "email", "age")
